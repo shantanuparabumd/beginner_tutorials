e65f2f008a3bf3f54545f1928093df39571a71bd
Shantanu Parab <sparab@umd.edu>
2022-10-30 21:10:19 -0400

    GCC Style

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -1,17 +1,13 @@
-// Copyright 2016 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+/**
+ * @file publisher_member_function.cpp
+ * @author Shantanu Parab (sparab@umd.edu)
+ * @brief Template for Publisuher ROS2
+ * @version 0.1
+ * @date 2022-10-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <chrono>
 #include <functional>
 #include <memory>
@@ -25,23 +21,20 @@ using namespace std::chrono_literals;
 /* This example creates a subclass of Node and uses std::bind() to register a
  * member function as a callback from the timer. */
 
-class MinimalPublisher : public rclcpp::Node
-{
-public:
+class MinimalPublisher : public rclcpp::Node {
+ public:
   MinimalPublisher()
-  : Node("minimal_publisher"), count_(0)
-  {
+  : Node("minimal_publisher"), count_(0) {
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
     timer_ = this->create_wall_timer(
       500ms, std::bind(&MinimalPublisher::timer_callback, this));
   }
 
-private:
-  void timer_callback()
-  {
+ private:
+  void timer_callback() {
     auto message = std_msgs::msg::String();
     message.data = "Hello, Shantanu! " + std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "ROS2 808X: '%s'", message.data.c_str());
+    RCLCPP_INFO(this->get_logger(), "808X ROS2: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
   rclcpp::TimerBase::SharedPtr timer_;
@@ -49,8 +42,7 @@ private:
   size_t count_;
 };
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char * argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalPublisher>());
   rclcpp::shutdown();
