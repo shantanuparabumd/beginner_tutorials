5324687865cf4be870cedc38a79a3b08d6391171
Shantanu Parab <sparab@umd.edu>
2022-11-06 17:02:42 -0500

    Adding Service and Creating a Server and Client

---
+++ src/modify_string_server.cpp
@@ -0,0 +1,30 @@
+#include "rclcpp/rclcpp.hpp"
+#include "beginner_tutorials/srv/modify_string.hpp"
+
+#include <iterator>
+#include <memory>
+
+void add(const std::shared_ptr<beginner_tutorials::srv::ModifyString::Request> request,
+          std::shared_ptr<beginner_tutorials::srv::ModifyString::Response>      response)
+{
+  response->c = request->a + request->b;
+
+  // RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\na: %ld" " b: %ld",
+  //               request->a, request->b);
+  // RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%ld]", (long int)response->sum);
+}
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+
+  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("modify_string_server");
+
+  rclcpp::Service<beginner_tutorials::srv::ModifyString>::SharedPtr service =
+    node->create_service<beginner_tutorials::srv::ModifyString>("modify_string", &add);
+
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to modify string.");
+
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+}
