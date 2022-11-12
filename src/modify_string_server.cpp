101c54c81ff01aa4943fc90c11aecd2d7fc0bd9b
Shantanu Parab <sparab@umd.edu>
2022-11-12 14:52:48 -0500

    Modifying string Using Services in Talker Node

--- src/modify_string_server.cpp
+++ src/modify_string_server.cpp
@@ -1,13 +1,16 @@
 #include "rclcpp/rclcpp.hpp"
 #include "beginner_tutorials/srv/modify_string.hpp"
-
+#include <cstdlib>
 #include <iterator>
+#include <string>
+#include <vector>
 #include <memory>
 
 void add(const std::shared_ptr<beginner_tutorials::srv::ModifyString::Request> request,
           std::shared_ptr<beginner_tutorials::srv::ModifyString::Response>      response)
 {
-  response->c = request->a + request->b;
+  std::vector<std::string> words{"Cat","Dog","Rat","Bat","Bird","Duck","Fox","Cow","Pig","Hen"};
+  response->c = request->a +" "+ request->b+" "+words[rand()%10];
 
   // RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\na: %ld" " b: %ld",
   //               request->a, request->b);
@@ -17,11 +20,10 @@ void add(const std::shared_ptr<beginner_tutorials::srv::ModifyString::Request> r
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
-
   std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("modify_string_server");
 
   rclcpp::Service<beginner_tutorials::srv::ModifyString>::SharedPtr service =
-    node->create_service<beginner_tutorials::srv::ModifyString>("modify_string", &add);
+  node->create_service<beginner_tutorials::srv::ModifyString>("modify_string", &add);
 
   RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to modify string.");
 
--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -15,6 +15,8 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "beginner_tutorials/srv/modify_string.hpp"
+using std::placeholders::_1;
 
 using namespace std::chrono_literals;
 
@@ -23,22 +25,50 @@ using namespace std::chrono_literals;
 
 class MinimalPublisher : public rclcpp::Node {
  public:
-  MinimalPublisher()
+  MinimalPublisher()  //Object for node is created using a constructor
   : Node("minimal_publisher"), count_(0) {
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
     timer_ = this->create_wall_timer(
       500ms, std::bind(&MinimalPublisher::timer_callback, this));
+    client = this->create_client<beginner_tutorials::srv::ModifyString>("modify_string");
+    while (!client->wait_for_service(1s)) {
+        if (!rclcpp::ok()) {
+          RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+          exit (EXIT_FAILURE);
+        }
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
+      }
   }
 
  private:
   void timer_callback() {
     auto message = std_msgs::msg::String();
-    message.data = "Hello, Shantanu! " + std::to_string(count_++);
+    message.data = "Hello, " +Message + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "808X ROS2: '%s'", message.data.c_str());
     publisher_->publish(message);
+    if(count_%10==0){
+      call_service();
+    }
   }
+  int call_service(){
+    auto request = std::make_shared<beginner_tutorials::srv::ModifyString::Request>();
+    request->a = "Random";
+    request->b = "Animal";
+    RCLCPP_INFO (this->get_logger(), "Callin Service to Modify string");
+    auto callbackPtr  = std::bind(&MinimalPublisher::response_callback, this, _1);
+    client->async_send_request(request, callbackPtr);
+    return 1;
+  }
+  void response_callback (rclcpp::Client<beginner_tutorials::srv::ModifyString>::SharedFuture future) {
+    // Process the response
+    RCLCPP_INFO (this->get_logger(), "Got String: %s", future.get()->c.c_str());
+    Message=future.get()->c.c_str();
+  }
+  std::string Message="Shantanu";
+  
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::Client<beginner_tutorials::srv::ModifyString>::SharedPtr client;
   size_t count_;
 };
 
